9f715fafc391f1d89444ce27cca9849d65e07679
Mark Ellis <markellis@quickenloans.com>
2020-02-03 23:02:06 -0500

    Lab 3.3 Data Structures

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Lab3._3_Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Hello, please enter a few words: ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine(text);
+
+           //Created a stack of Char
+            Stack<char> stack = new Stack<char>();
+
+            
+            //Used foreach loop to push each character in text string to stack.
+            foreach (char c in text)
+            {
+                stack.Push(c);
+            }
+            
+            //used string.Empty to clear value in text.
+            text = string.Empty;
+
+            //Used a while loop to pop each character in the stack back to text string.
+            while (stack.Count > 0)
+            {
+                text += stack.Pop();
+            }
+
+            Console.WriteLine(text);
+        }
+
+    }
+
+}
